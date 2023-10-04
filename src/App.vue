ac4125810ab266db609f453497f171f7602a574a
Rohail Ramesh <rohailramesh@hotmail.com>
2023-10-04 12:05:03 +0100

    Second commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HomePage
+    data="Rohail Ramesh"
+    message="This is the homepage to my website."
+  />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomePage from "./components/HomePage.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HomePage,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>Hello from the home component</h1>
+    <h2>{{ data }}</h2>
+    <p>{{ message }}</p>
+    <button v-on:click="homePageFunc">Home Page</button>
+    <button v-on:click="alert('You clicked the alert button')">Alert</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+  props: { data: String, message: String },
+  methods: {
+    homePageFunc() {
+      console.log("This is the homePageFunc");
+    },
+    alert(data) {
+      window.alert(data);
+    },
+  },
+};
+</script>
+
+<style>
+button {
+  display: block;
+  margin-bottom: 10px;
+  justify-content: center;
+  justify-items: center;
+  align-content: center;
+  align-items: center;
+  display: flex;
+}
+</style>
